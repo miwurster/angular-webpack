9d480ea5f6c2fa3cf0035769d4f95f15217d45fd
miwurster <miwurster@googlemail.com>
2017-01-30 17:16:08 +0100

    Improved unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,35 @@
-import { TestBed }             from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 
-describe('App', () => {
+describe('First Test', () => {
+  it('is true', () => expect(true).toBe(true));
+});
+
+describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-      imports: [
-        RouterTestingModule
-      ],
+      declarations: [ AppComponent ],
+      imports: [ RouterTestingModule ],
     });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
-  it ('should work', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent)
-      .toBe(true, 'should create AppComponent');
+
+  it('is of type AppComponent', () => {
+    expect(component instanceof AppComponent).toBe(true);
+  });
+
+  it('has a title', () => {
+    let debug: DebugElement = fixture.debugElement.query(By.css('.navbar-brand'));
+    let element: HTMLElement = debug.nativeElement;
+    expect(element.textContent).toContain('Tour of Heroes');
   });
 });
